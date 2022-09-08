29a507669486f70a9cbc05561df5091877af7067
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-08 11:15:26 +0930

    what is linq

---
+++ 01_WhatIsLinq/01_WhatIsLinq/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_WhatIsLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = {1, 2, 3, 7, 9, 4, 6, 5, 8 };
+            int[] oddNums = GetOddNums(ints);
+
+            foreach (int i in oddNums)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+
+        static int[] GetOddNums(int[] ints)
+        {
+            IEnumerable<int> OddNums = from i in ints where i % 2 != 0 select i;
+
+            return OddNums.ToArray<int>();
+        }
+    }
+}
